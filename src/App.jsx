cbf3fc83c5e4b8692420a0a4134946da7642d058
win <renzouyomu@gmail.com>
2023-10-25 14:36:02 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimRouter from "./components/AnimRouter";
-import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Router>
-        <Header />
         <AnimRouter />
       </Router>
     </>
