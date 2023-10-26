66af2cb752140945d2802c026b4a1a81d1c97416
win <renzouyomu@gmail.com>
2023-10-26 23:13:58 +0800

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimRouter from "./components/AnimRouter";
 import Navbar from "./components/Navbar";
-
-
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
       <Router>
         <Navbar />
         <AnimRouter />
+        <Footer />
       </Router>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex py-2 justify-center text-xl font-semibold">
+      <h1>&copy; Final Project Kelompok 5</h1>
+    </div>
+  );
+};
+
+export default Footer;
