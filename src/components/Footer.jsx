3e70ec860e9ddc4f017136df675ebddcde1659e4
Farhanmuh69 <fmuh748@gmail.com>
2023-10-31 10:44:35 +0700

    responsive-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,12 +11,11 @@ import {
 
 const Footer = () => {
   return (
-    <div className="mt-10 bg-black text-[#949494] py-20 font-tittleFont">
-      <div className="max-w-screen-xl mx-auto grid grid-cols-4">
-        {/* LogoIcon Start here */}
-        <div className="flex flex-col gap-7">
+    <div className="mt-10 bg-black text-[#949494] py-10 px-4 sm:py-20 font-tittleFont">
+      <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="md:col-span-2 lg:col-span-1 flex flex-col gap-4">
           <p className="text-white text-2xl font-semibold">© Team-V</p>
-          <div className="flex gap-5 text-lg text-gray-400">
+          <div className="flex gap-4 text-lg text-gray-400">
             <ImGithub className="hover:text-white duration-300 cursor-pointer" />
             <FaYoutube className="hover:text-white duration-300 cursor-pointer" />
             <FaFacebookF className="hover:text-white duration-300 cursor-pointer" />
@@ -24,9 +23,7 @@ const Footer = () => {
             <FaInstagram className="hover:text-white duration-300 cursor-pointer" />
           </div>
         </div>
-        {/* LogoIcon End here */}
-        {/* LocateUs Start here */}
-        <div>
+        <div className="md:col-span-1 lg:col-span-1">
           <h2 className="text-2xl font-semibold text-white mb-4">About us</h2>
           <div className="text-base flex flex-col gap-2">
             <p>RCTN-KS08-010 - Iqbal</p>
@@ -34,12 +31,8 @@ const Footer = () => {
             <p>RCTN-KS08-017 - El</p>
           </div>
         </div>
-        {/* LocateUs End here */}
-        {/* Profile Start here */}
-        <div>
-          <h2 className="text-2xl font-semibold text-white mb-4">
-            Helpful Links
-          </h2>
+        <div className="md:col-span-1 lg:col-span-1">
+          <h2 className="text-2xl font-semibold text-white mb-4">Helpful Links</h2>
           <div className="flex flex-col gap-2 text-base">
             <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
               <span>
@@ -67,22 +60,22 @@ const Footer = () => {
             </p>
           </div>
         </div>
-        {/* Profile End here */}
-        {/* Subcribe Start here */}
-        <div className="flex flex-col justify-center">
-          <input
-            className="bg-transparent border px-4 py-2 text-sm"
-            placeholder="e-mail"
-            type="text"
-          />
-          <button className="text-sm border text-white border-t-0 hover:bg-gray-900 active:bg-white active:text-black">
-            Subscribe
-          </button>
+        <div className="md:col-span-2 lg:col-span-1">
+          <div className="flex flex-col md:flex-row md:justify-between">
+            <input
+              className="bg-transparent border px-4 py-2 text-sm md:w-2/3"
+              placeholder="e-mail"
+              type="text"
+            />
+            <button className="text-sm border text-white border-t-0 hover:bg-gray-900 active:bg-white active:text-black md:w-1/3 mt-2 md:mt-0">
+              Subscribe
+            </button>
+          </div>
         </div>
-        {/* Subcribe End here */}
       </div>
     </div>
   );
 };
 
 export default Footer;
+
