15987440b2ca10ada6b19b5451b803d1b67c0be3
win <renzouyomu@gmail.com>
2023-11-15 13:03:32 +0800

    add redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import { persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import covidReducers from "./reducers/covid";
 import indonesiaReducers from "./reducers/indonesia";
 import programmingReducers from "./reducers/programming";
 import homeReducers from "./reducers/home";
 import savedNewsReducers from "./reducers/save";
 
-const store = configureStore({
-  reducer: {
-    covid: covidReducers,
-    indonesia: indonesiaReducers,
-    programming: programmingReducers,
-    home: homeReducers,
-    savedNews: savedNewsReducers,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  covid: covidReducers,
+  indonesia: indonesiaReducers,
+  programming: programmingReducers,
+  home: homeReducers,
+  savedNews: savedNewsReducers,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+const persistor = persistStore(store);
+
+export { store, persistor };
